282a3668254be0323e910021841bde8ec07d0be4
gamechanger121 <118898201+gamechanger121@users.noreply.github.com>
2023-05-04 05:44:19 +0530

    Update 12robin.c

--- 12robin.c
+++ 12robin.c
@@ -1,73 +1,53 @@
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <string.h>
 
-#define d 256
-
-void search(char* pat,char *txt,int q)
+void search(char pat[], char txt[], int q)
 {
     int M = strlen(pat);
     int N = strlen(txt);
-    int i,j;
-    int p = 0; 
-    int t = 0;
-    int h = 1;
+    int p = 0, t = 0, h = 1, d = 256;
+    int i, j;
 
-    for(i = 0;i < M-1;i++)
-    {
-        h = (h*d)%q ;
-    }
-    
-    for(i = 0;i < M;i++)
-    {
-        p = (d * p +pat[i]) % q;
-        t = (d * p +txt[i]) % q;
+    for (i = 0; i < M - 1; i++) {
+        h = (h * d) % q;
     }
 
-    for(i = 0;i <= N-M; i++)
-    {
-        if(p == t)
-        {
-            for (j = 0; j < M; j++)
-            {
-                if(txt[i+j] != pat[j])
-                {
+    for (i = 0; i < M; i++) {
+        p = (d * p + pat[i]) % q;
+        t = (d * t + txt[i]) % q;
+    }
+
+    for (i = 0; i <= N - M; i++) {
+        if (p == t) {
+            for (j = 0; j < M; j++) {
+                if (txt[i + j] != pat[j]) {
                     break;
                 }
             }
-
-            if(j == M)
-            {
-                printf("Pattern found at index %d \n", i);
+            if (j == M) {
+                printf("\nPattern found at index %d \n", i);
             }
-            
         }
-        if (i < N - M) 
-        {
+
+        if (i < N - M) {
             t = (d * (t - txt[i] * h) + txt[i + M]) % q;
 
-            
-            if (t < 0)
+            if (t < 0) {
                 t = (t + q);
+            }
         }
-
     }
 }
 
 int main()
 {
-    char txt[1000], pat[1000];
-    int q;
-    
-    printf("Enter the text: ");
-    scanf("%[^\n]%*c", txt);
-    
-    printf("Enter the pattern: ");
-    scanf("%[^\n]%*c", pat);
-    
-    printf("Enter a prime number: ");
-    scanf("%d", &q);
-
+    char txt[100], pat[100];
+    printf("\nEnter the text: ");
+    scanf("%[^\n]s",txt);
+    getchar();
+    printf("\nEnter the pattern: ");
+    scanf("%[^\n]s",pat);
+    int q = 101;
     search(pat, txt, q);
     return 0;
 }
